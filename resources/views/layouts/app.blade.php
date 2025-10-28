d518b48acfe1acefeb6d9bf93238298bb5a18aed
sorayda-san09 <soraydasantinhernandez@gmail.com>
2025-10-28 09:39:49 -0700

    No me lo borres Ochoa

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -75,6 +75,31 @@
                     </ul>
                 </li>
 
+                <li class="dropdown-item" x-data="{ open: false }">
+        <!-- Eliminamos el <span> extra del título aquí, el nombre ya está en <a> --><a href="#" @click="open = !open" class="dropdown-link">
+                        <i class="fas fa-money-check-alt"></i>
+                        <span>Nómina</span> 
+                        <svg class="dropdown-arrow" x-bind:style="{ 'transform': open ? 'rotate(180deg)' : 'rotate(0deg)' }" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                            <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                        </svg>
+                    </a>
+                        <ul x-show="open" class="profile-submenu" style="display: none;" x-transition>
+                            <li>
+                                <a href="{{ route('pagos.create') }}">
+                                    <i class="fas fa-user-plus"></i> Registrar pago
+                                </a>
+                            </li>
+                            <li>
+                                <a href="{{ route('pagos.lista') }}">
+                                    <i class="fas fa-users"></i> Lista de pagos
+                                </a>
+                            </li>
+                         
+                           
+                          
+                        </ul>
+                    </li>
+
                 {{-- Tareas Dropdown --}}
                 <li class="dropdown-item" x-data="{ open: false }">
                     <a href="#" @click="open = !open" class="dropdown-link">
