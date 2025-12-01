aa0c2c297f4319b50d5b01c3dbabfdfd8d5a96c5
Arturo AL <luisarce14127@gmail.com>
2025-12-01 16:47:35 -0700

    Hola2

--- resources/views/clientes/inf_clientes.blade.php
+++ resources/views/clientes/inf_clientes.blade.php
@@ -7,7 +7,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    <h1 class="text-2xl font-bold">Gestión de servicios</h1>
+                    <h1 class="text-2xl font-bold">Gestión de clientes para servicios</h1>
                     <br>
 
                     @if($contactos->isEmpty())
